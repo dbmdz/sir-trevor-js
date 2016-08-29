d5e75badb84631d4a1a45df4ba042b8f494157aa
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2016-08-29 11:10:56 +0200

    Added a fixed width to the pixel input field.

--- src/blocks/image-gallery.js
+++ src/blocks/image-gallery.js
@@ -24,7 +24,7 @@ module.exports = Block.extend({
            '<div class="imageFormatting">' +
            '  <input type="checkbox" name="' + blockId + '-normalize-height" data-name="normalize-height" />' +
            '  Bildhöhe auf' +
-           '  <input type="text" name="' + blockId + '-height" data-name="height"/> Pixel vereinheitlichen' +
+           '  <input class="normalized-height" type="text" name="' + blockId + '-height" data-name="height"/> Pixel vereinheitlichen' +
            '</div>' +
            '</div>';
   },
