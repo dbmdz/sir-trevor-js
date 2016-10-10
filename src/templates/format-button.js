c2c1512c5365d2fb4d60a0c36c90ae01f7091670
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2016-10-10 10:44:44 +0200

    Added a title attribute to every format button for displaying a description in a tooltip.

--- src/templates/format-button.js
+++ src/templates/format-button.js
@@ -2,9 +2,10 @@
 
 var config = require('../config');
 
-module.exports = function({name, text, cmd, iconName}) {
+module.exports = function({name, title, cmd, iconName}) {
   return `
-    <button type="button" class="st-format-btn st-format-btn--${name}" data-cmd="${cmd}">
+    <button type="button" class="st-format-btn st-format-btn--${name}"
+      title="${i18n.t('formatButtons:' + title)}" data-cmd="${cmd}">
       <svg role="img" class="st-icon">
         <use xlink:href="${config.defaults.iconUrl}#${iconName}"/>
       </svg>
