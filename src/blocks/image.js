cc02e0bd24b2f7e8184e89bbea59f9b30e161af2
Matthias Lindinger <matthias.lindinger@bsb-muenchen.de>
2016-10-27 14:21:15 +0200

    Added using of existing objects as image.

--- src/blocks/image.js
+++ src/blocks/image.js
@@ -6,8 +6,8 @@ var Block = require('../block');
 module.exports = Block.extend({
 
   type: 'image',
-
   icon_name: 'image',
+  fetchable: true,
 
   editorHTML: function () {
     var blockId = this.blockID;
@@ -31,14 +31,15 @@ module.exports = Block.extend({
            '    <b>Bild-Link:</b>' +
            '    <input type="text" class="form-control" name="' + blockId + '-image-link" data-name="image-link" />' +
            '  </div>' +
-           '  <div class="col-lg-4 col-md-4 imagePreview"></div>' +
+           '  <div class="col-lg-4 col-md-4 imageDisplay">' +
+           '    <div class="imagePreview"></div>' +
+           '  </div>' +
            '</div>';
   },
 
   loadData: function(data){
     // Create our image tag
-    var imagePreview = Dom.createElement('img', {src: '/uploads/' + data.id + '/thumbnail', class: 'thumbnail'});
-    this.inner.querySelector('.imagePreview').appendChild(imagePreview);
+    this.insertThumbnail(data);
 
     // Set the radio button value for position
     if(data.position){
@@ -60,17 +61,75 @@ module.exports = Block.extend({
         // Remove any error messages
         this.resetMessages();
         // Add the appropriate id or display an error message and exit the process
-        if(data.imageId !== ''){
-          this.setData({'id': data.imageId});
-        }else{
+        if(data.objectID === '' && data.imageId === ''){
           return this.addMessage('Es wurde keine ID angegeben.');
+        }else{
+          this.insertThumbnail(data);
         }
-
-        Dom.remove(this.inner.querySelector('.imagePreview > img'));
-        var imagePreview = Dom.createElement('img', {src: '/uploads/' + data.imageId + '/thumbnail', class: 'thumbnail'});
-        this.inner.querySelector('.imagePreview').appendChild(imagePreview);
       }.bind(this));
       $('#extendedUploadModal').modal('show');
     }.bind(this));
+  },
+
+  fetchUrl: function(id){
+    if(id.slice(0, 3) === 'bsb'){
+      id = 'BSB-MDZ-00000BSB' + id.slice(3);
+    }
+    return '/api/artifacts/' + id;
+  },
+
+  onFetchSuccess: function(data){
+    // Get the thumbnail corresponding to the given id
+    var thumbnailImage = this.inner.querySelector('.imagePreview > img');
+    // Create the image tag and append it to the thumbnail div
+    thumbnailImage.setAttribute('src', '/iiif/' + data.previewId + '/full/200,/0/native.jpg');
+    this.setData({'id': {
+      'type': 'object',
+      'value': data.previewId
+    }});
+    this.ready();
+  },
+
+  onFetchFail: function(){
+    // Display an error message
+    this.addMessage('Objekt konnte nicht geladen werden.');
+    this.ready();
+  },
+
+  fetchData: function(objectId){
+    // Remove any error messages
+    this.resetMessages();
+    // Fetch information on the object
+    this.fetch(
+      this.fetchUrl(objectId), { dataType: 'json' },
+      this.onFetchSuccess.bind(this),
+      this.onFetchFail.bind(this)
+    );
+  },
+
+  insertThumbnail: function(data){
+    var thumbnailWrapper = this.inner.querySelector('.imagePreview');
+
+    thumbnailWrapper.classList.add('thumbnail');
+
+    if(data.objectID && data.objectID !== ''){
+      thumbnailWrapper.innerHTML = '<img class="img-responsive" src="/img/preloader.gif">';
+      this.fetchData(data.objectID);
+    }else if(data.imageId && data.imageId !== ''){
+      this.setData({'id': {
+        'type': 'image',
+        'value': data.imageId
+      }});
+      thumbnailWrapper.innerHTML = '<img class="img-responsive" src="/uploads/' + data.imageId + '/thumbnail">';
+    }else if(data.id && typeof data.id === 'string'){
+      thumbnailWrapper.innerHTML = '<img class="img-responsive" src="/uploads/' + data.id + '/thumbnail">';
+    }else if(data.id && typeof data.id === 'object'){
+      if(data.id.type === 'image'){
+        thumbnailWrapper.innerHTML = '<img class="img-responsive" src="/uploads/' + data.id.value + '/thumbnail">';
+      }else if(data.id.type === 'object'){
+        thumbnailWrapper.innerHTML = '<img class="img-responsive" src="/img/preloader.gif">';
+        this.fetchData(data.id.value);
+      }
+    }
   }
 });
